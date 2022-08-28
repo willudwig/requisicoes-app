d7127a4dbcf379882586e38bfa23221739733ec8
William Ludwig <acesso.william@gmail.com>
2022-08-28 16:05:16 -0300

    terceiro

--- src/app/departamentos/departamento.component.ts
+++ src/app/departamentos/departamento.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+import { Departamento } from './models/departamento.model';
+import { DepartamentoService } from './services/departamento.service';
 
 @Component({
   selector: 'app-departamento',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./departamento.component.css']
 })
 export class DepartamentoComponent implements OnInit {
+  public departamentos$: Observable<Departamento[]>;
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(private departamentoService: DepartamentoService,
+              private fb: FormBuilder,
+              private modalServie: NgbModal
+             ) { }
 
   ngOnInit(): void {
+    this.departamentos$ = this.departamentoService.selecionarTodos();
+
+    this.form = this.fb.group({
+      id: new FormControl(""),
+      nome: new FormControl(""),
+      telefone: new FormControl("")
+    });
+
+  }
+
+  get tituloModal(): string {
+    return this.id?.value ? "Atualização" : "Cadastro";
+  }
+
+  get id() {
+    return this.form.get("id");
+  }
+
+  get nome() {
+    return this.form.get("nome");
+  }
+
+  get telefone() {
+    return this.form.get("telefone");
+  }
+
+  public async gravar(modal: TemplateRef<any>, departamento?: Departamento) {
+    this.form.reset;
+
+    if(departamento)
+      this.form.setValue(departamento);
+
+    try {
+      await this.modalServie.open(modal).result;
+
+      if(!departamento)
+        await this.departamentoService.inserir(this.form.value);
+      else
+        await this.departamentoService.editar(this.form.value);
+
+      console.log(`O departamento foi salvo com sucesso`);
+
+    } catch (_error) {
+
+    }
+
+  }
+
+  public excluir(departamento: Departamento) {
+    return this.departamentoService.excluir(departamento);
   }
 
 }
--- src/app/departamentos/services/departamento.service.ts
+++ src/app/departamentos/services/departamento.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import { Departamento } from '../models/departamento.model';
 
 @Injectable({
@@ -7,7 +8,36 @@ import { Departamento } from '../models/departamento.model';
 })
 export class DepartamentoService {
 
-  registros: AngularFirestoreCollection<Departamento>;
+  private registros: AngularFirestoreCollection<Departamento>;
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) {
+      this.registros = this.firestore.collection<Departamento>("departamentos");
+
+   }
+
+   public selecionarTodos(): Observable<Departamento[]> {
+     return this.registros.valueChanges();
+
+   }
+
+   public async inserir(registro: Departamento): Promise<any> {
+      if(!registro) {
+        return Promise.reject("Item inválido");
+      }
+
+      const res = await this.registros.add(registro);
+      registro.id = res.id;
+      this.registros.doc(res.id).set(registro);
+
+   }
+
+   public async editar(registro: Departamento): Promise<void> {
+      return this.registros.doc(registro.id).set(registro);
+
+   }
+
+   public async excluir(registro: Departamento): Promise<void> {
+      this.registros.doc(registro.id).delete();
+
+   }
 }
