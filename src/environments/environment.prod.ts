6e3c88ddc32514d059b5a3d95fdeae48ca261d7b
William Ludwig <acesso.william@gmail.com>
2022-09-13 17:49:45 -0300

    enviroment, prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXAzahhyddbfGk3LjXjrmMB3qPnKlauu0",
+    authDomain: "requisicoes-app-6b7d0.firebaseapp.com",
+    projectId: "requisicoes-app-6b7d0",
+    storageBucket: "requisicoes-app-6b7d0.appspot.com",
+    messagingSenderId: "1036372623886",
+    appId: "1:1036372623886:web:f2263ad6463de7d90e725f"
+  }
 };
